2870735e08bffb056551663b402e092148c955ba
myleslangston <84974498+myleslangston@users.noreply.github.com>
2021-06-21 14:26:20 +0100

    Time Function with Waiting List changes

--- time_function.py
+++ time_function.py
@@ -1,17 +1,33 @@
 import time
+from random import randint
+
 
 def time_counter(t):
-
+    waiting_list = []
+    identification = 1
+    in_training = []
     while t != 0:
         years, months = divmod(t, 12)
         timer = '{:02d}:{:02d}'.format(years, months)
         print(timer, end="\r")
         print(t)
         time.sleep(1)
+        x = 0
+        new_people = randint(20, 30)
+        attend_centre = randint(0, 20)
+        for i in range(0, new_people):
+            waiting_list.append(identification)
+            identification += 1
+        print(new_people)
+        for x in range(0, attend_centre):
+            into_training = waiting_list.pop(0)
+            in_training.append(into_training)
+        print(attend_centre)
+        print(waiting_list)
+        print(in_training)
         t -= 1
-
     print("The time simulation is over")
 
-t = input("Enter the time in months for the programme to run: ")
 
+t = input("Enter the time in months for the programme to run: ")
 time_counter(int(t))
