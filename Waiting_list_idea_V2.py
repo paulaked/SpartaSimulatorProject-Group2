1819576eeb30aebded589c4592f96bd5b1a2ca74
milan-crypto <85555923+milan-crypto@users.noreply.github.com>
2021-06-22 14:04:43 +0100

    Updated

--- Waiting_list_idea_V2.py
+++ Waiting_list_idea_V2.py
@@ -1,14 +1,14 @@
 import random
 
-first_name = {"jay", "jim", "roy", "axel", "billy", "charlie", "jax", "gina", "paul", "ringo", "ally", "nicky", "cam",
-              "ari", "trudie", "cal", "carl", "lady", "lauren", "ichabod", "arthur", "ashley", "drake", "kim", "julio",
-              "lorraine", "floyd", "janet", "lydia", "charles", "pedro", "bradley"}
-last_name = {"barker", "style", "spirits", "murphy", "blacker", "bleacher", "rogers", "warren", "keller"}
+fname = {"jay", "jim", "roy", "axel", "billy", "charlie", "jax", "gina", "paul", "ringo", "ally", "nicky", "cam",
+         "ari", "trudie", "cal", "carl", "lady", "lauren", "ichabod", "arthur", "ashley", "drake", "kim", "julio",
+         "lorraine", "floyd", "janet", "lydia", "charles", "pedro", "bradley"}
+lname = {"barker", "style", "spirits", "murphy", "blacker", "bleacher", "rogers", "warren", "keller"}
 
 
-def Name(first_name, last_name):
-    first = random.sample(first_name, 1)[0]
-    last = random.sample(last_name - {first}, 1)[0]
+def Name(f_name, l_name):
+    first = random.sample(f_name, 1)[0]
+    last = random.sample(l_name - {first}, 1)[0]
     return first + " " + last
 
 
@@ -16,7 +16,7 @@ OUT = []
 
 i = 1000
 while i > 0:
-    name = Name(first_name, last_name)
+    name = Name(fname, lname)
     if name not in OUT:
         OUT.append(name)
         print(name)
